30896883b35878bb05b9c211e1d568cee5666a8c
z2s <zen2see@gmail.com>
2019-06-30 19:28:21 -0400

    Try converting emit parameter of LogDepositMade to BN.js

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -103,7 +103,7 @@ contract SimpleBank {
           );
 
           balances[msg.sender] += msg.value;
-          emit LogDepositMade(msg.sender, msg.value);
+          emit LogDepositMade(msg.sender, web3.utilstoBN(msg.value));
           return balances[msg.sender];
     }
 
